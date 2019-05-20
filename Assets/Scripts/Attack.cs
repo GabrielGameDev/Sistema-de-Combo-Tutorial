3a8de1c89fb7fe5542a9f0e4e7f07768fee22241
Gabriel-PC\Gabriel <gabrielbarbosa@msn.com>
2019-05-20 11:13:55 -0300

    Commit
    Commit

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+	public AudioPlayer audioPlayer;
+
+	private int damage;
+	private bool slowDown;
+	private AudioClip hitSound;
+
+	public void SetAttack(Hit hit)
+	{
+		damage = hit.damage;
+		slowDown = hit.slowDown;
+		hitSound = hit.hitSound;
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		Damage enemy = other.GetComponent<Damage>();
+		if(enemy != null)
+		{
+			enemy.TakeDamage(damage);
+			audioPlayer.PlaySound(hitSound);
+			if (slowDown)
+				SlowDown.instance.SetSlowDown();
+
+			ComboManager.instance.SetCombo();
+		}
+	}
+}
